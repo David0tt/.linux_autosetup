4e291a3f3e4842e0d97dc35b38f45e529cfb6d07
David Ott <david.ott@uni-tuebingen.de>
2026-05-25 17:09:06 +0200

    make fish behave a little more like bash

--- config_files/fish/config.fish
+++ config_files/fish/config.fish
@@ -2,9 +2,12 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 # overwrite greeting
 # potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+function fish_greeting
+end
+
+set -g pure_symbol_prompt ">"
+set -g pure_symbol_reverse_prompt "<"
+
 
 export TERMINAL=alacritty
 
@@ -14,5 +17,18 @@ set -x FZF_CTRL_R_OPTS "--with-nth=3.." # Only show the commands (not the time-s
 
 alias grep="rg"
 alias find="fd"
+
+function __newline_cancel_commandline
+	if test -n "$(commandline)"
+		commandline -f cancel-commandline
+	else
+		echo
+		commandline -f repaint
+	end
+end
+
+if status is-interactive
+	bind \cc __newline_cancel_commandline
+end
 
 
